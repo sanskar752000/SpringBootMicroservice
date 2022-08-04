895a3df0669c6de3c71da01068ebabe6bc2e052a
sanskar752000 <sanskar752000@gmail.com>
2022-08-04 10:32:37 +0530

    changed Repository extends
    Changed from CrudRepository to PagingAndSortingRepository to achieve the paging and sorting functionality for the endpoint

--- SpringMicroservice/src/main/java/com/example/springmicro/springmicroservice/repository/TourRepository.java
+++ SpringMicroservice/src/main/java/com/example/springmicro/springmicroservice/repository/TourRepository.java
@@ -1,10 +1,13 @@
 package com.example.springmicro.springmicroservice.repository;
 
 import com.example.springmicro.springmicroservice.domain.Tour;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.util.List;
 
-public interface TourRepository extends CrudRepository<Tour, Integer> {
-    List<Tour> findByTourPackageCode(String code);
+public interface TourRepository extends PagingAndSortingRepository<Tour, Integer> {
+    Page<Tour> findByTourPackageCode(String code, Pageable pageable);
 }
