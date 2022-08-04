9dd243e153418d4f95392b03d8a0a201f9c13e47
sanskar752000 <sanskar752000@gmail.com>
2022-08-04 10:16:24 +0530

    Added method in TourRepository
    Added new method in TourRepository to find all the tours associated with the Tour Package Code.

--- SpringMicroservice/src/main/java/com/example/springmicro/springmicroservice/repository/TourRepository.java
+++ SpringMicroservice/src/main/java/com/example/springmicro/springmicroservice/repository/TourRepository.java
@@ -3,5 +3,8 @@ package com.example.springmicro.springmicroservice.repository;
 import com.example.springmicro.springmicroservice.domain.Tour;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface TourRepository extends CrudRepository<Tour, Integer> {
+    List<Tour> findByTourPackageCode(String code);
 }
