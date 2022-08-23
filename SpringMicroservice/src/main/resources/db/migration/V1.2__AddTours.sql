f3d74dcbaed0bfaa2455713590d03cbe0cbb5f2f
sanskar752000 <sanskar752000@gmail.com>
2022-08-23 16:37:46 +0530

    Added flyway DB dependency
    Added Flyway DB dependency to prevent creation of tables data duplicated every time application starts.

--- SpringMicroservice/src/main/resources/db/migration/V1.2__AddTours.sql
+++ SpringMicroservice/src/main/resources/db/migration/V1.2__AddTours.sql
@@ -1,14 +1,3 @@
-insert into tour_package (code, name)
-values ('BC', 'Backpack Cal'),
-       ('CC', 'California Calm'),
-       ('CH', 'California Hot springs'),
-       ('CY', 'Cycle California'),
-       ('DS', 'From Desert to Sea'),
-       ('KC', 'Kids California'),
-       ('NW', 'Nature Watch'),
-       ('SC', 'Snowboard Cali'),
-       ('TC', 'Taste of California');
-
 insert into tour (tour_package_code, title, description, blurb, bullets, difficulty, duration, price, region, keywords)
 values ('BC',
         'Big Sur Retreat',
@@ -328,29 +317,3 @@ values ('TC',
         '850',
         'Napa/Sonoma Counties',
         'Winery, hiking, relaxing, cooking, Napa Valley');
-
-insert into tour_rating (tour_id, customer_id, score, comment)
-values (1, 4, 5, 'I loved it'),
-       (2, 100, 5, 'I really thought it could have been better');
-
--- USER
--- non-encrypted password: letmein
-INSERT INTO security_user (id, username, password, first_name, last_name)
-VALUES (1, 'admin', '$2a$12$ZhGS.zcWt1gnZ9xRNp7inOvo5hIT0ngN7N.pN939cShxKvaQYHnnu', 'Administrator', 'Adminstrator'),
-       (2, 'csr_jane', '$2a$12$ZhGS.zcWt1gnZ9xRNp7inOvo5hIT0ngN7N.pN939cShxKvaQYHnnu', 'Jane', 'Doe'),
-       (3, 'csr_mark', '$2a$12$ZhGS.zcWt1gnZ9xRNp7inOvo5hIT0ngN7N.pN939cShxKvaQYHnnu', 'Mark', 'Smith'),
-       (4, 'wally', '$2a$12$ZhGS.zcWt1gnZ9xRNp7inOvo5hIT0ngN7N.pN939cShxKvaQYHnnu', 'Walter', 'Adams');
-
--- ROLES
-
-INSERT INTO security_role (id, role_name, description)
-VALUES (1, 'ROLE_ADMIN', 'Administrator');
-INSERT INTO security_role (id, role_name, description)
-VALUES (2, 'ROLE_CSR', 'Customer Service Representative');
-
-INSERT INTO user_role(user_id, role_id)
-VALUES (1, 1), -- give admin ROLE_ADMIN
-       (2, 2), -- give Jane ROLE_CSR
-       (3, 2), -- give Mark ROLE_CSR
-       (4, 1), -- give Wally ROLE_ADMIN
-       (4, 2); -- give Wally ROLE_CSR
